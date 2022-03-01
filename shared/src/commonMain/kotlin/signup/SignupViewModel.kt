e3b77f59fa82124419a148ab339c756ceb940314
Alex Huang <alex@yolabs.io>
2022-03-01 14:07:23 -0800

    Map and flatMap flows

--- shared/src/commonMain/kotlin/signup/SignupViewModel.kt
+++ shared/src/commonMain/kotlin/signup/SignupViewModel.kt
@@ -1,13 +1,18 @@
 package signup
 
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
-import io.ktor.client.*
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import network.GitHubApi
 import network.GitHubValidationService
+import network.PasswordValidationResult
+import network.RepeatedPasswordValidationResult
+import network.UsernameValidationResult
 
 class SignupViewModel(
     private val gitHubApi: GitHubApi,
@@ -16,14 +21,81 @@ class SignupViewModel(
 
     private val _username = MutableStateFlow("")
     private val _password = MutableStateFlow("")
-    private val _passwordRepeat = MutableStateFlow("")
+    private val _repeatedPassword = MutableStateFlow("")
+    private val _onSignUpButtonClicked = MutableSharedFlow<Unit>()
+    private val _signingUp = MutableStateFlow(false)
 
-    val username = _username.asStateFlow()
-    val password = _password.asStateFlow()
-    val passwordRepeat = _passwordRepeat.asStateFlow()
-    val isUsernameValidationHidden = username.map { it.isEmpty() }
-    val isPasswordValidationHidden = password.map { it.isEmpty() }
-    val isPasswordRepeatValidationHidden = passwordRepeat.map { it.isEmpty() }
+    private val usernameValidationResult = _username.flatMapLatest { username ->
+        gitHubValidationService.validateUsername(username)
+    }
+    private val passwordValidationResult = _password.map { password ->
+        gitHubValidationService.validatePassword(password)
+    }
+    private val repeatedPasswordValidationResult = _password.combine(_repeatedPassword) { password, repeatedPassword ->
+        gitHubValidationService.validateRepeatedPassword(password, repeatedPassword)
+    }
+
+    val isSignInButtonEnabled = combine(
+        usernameValidationResult,
+        passwordValidationResult,
+        repeatedPasswordValidationResult,
+        _signingUp
+    ) { result1, result2, result3, signingUp ->
+        result1 == UsernameValidationResult.OK &&
+                result2 == PasswordValidationResult.OK &&
+                result3 == RepeatedPasswordValidationResult.OK &&
+                !signingUp
+    }
+        .distinctUntilChanged()
+
+    val usernameValidationText = usernameValidationResult.map { result ->
+        when (result) {
+            UsernameValidationResult.EMPTY ->
+                ""
+            UsernameValidationResult.VALIDATING ->
+                "Validating username"
+            UsernameValidationResult.OK ->
+                "Username available"
+            UsernameValidationResult.WRONG_FORMAT ->
+                "Username can only contain numbers or digits"
+            UsernameValidationResult.ALREADY_TAKEN ->
+                "Username already taken"
+        }
+    }
+
+    val passwordValidationText = passwordValidationResult.map { result ->
+        when (result) {
+            PasswordValidationResult.EMPTY ->
+                ""
+            PasswordValidationResult.OK ->
+                "Password acceptable"
+            PasswordValidationResult.TOO_SHORT ->
+                "Password must be at least ${gitHubValidationService.minPasswordCount} characters"
+        }
+    }
+
+    val repeatedPasswordValidationText = repeatedPasswordValidationResult.map { result ->
+        when (result) {
+            RepeatedPasswordValidationResult.EMPTY ->
+                ""
+            RepeatedPasswordValidationResult.OK ->
+                "Password repeated"
+            RepeatedPasswordValidationResult.DIFFERENT ->
+                "Password different"
+        }
+    }
+
+    val isUsernameValidationLabelHidden = usernameValidationText.map { it.isEmpty() }
+    val isPasswordValidationLabelHidden = passwordValidationText.map { it.isEmpty() }
+    val isRepeatedPasswordValidationLabelHidden = repeatedPasswordValidationText.map { it.isEmpty() }
+
+    val signedIn = _onSignUpButtonClicked.flatMapLatest {
+        flow {
+            _signingUp.value = true
+            emit(gitHubApi.signUp(_username.value, _password.value))
+            _signingUp.value = false
+        }
+    }
 
     fun onUsernameChanged(username: String) {
         _username.value = username
@@ -33,8 +105,12 @@ class SignupViewModel(
         _password.value = password
     }
 
-    fun onPasswordRepeatChanged(passwordRepeat: String) {
-        _passwordRepeat.value = passwordRepeat
+    fun onRepeatedPasswordChanged(repeatedPassword: String) {
+        _repeatedPassword.value = repeatedPassword
+    }
+
+    fun onSignInButtonClicked() {
+        _onSignUpButtonClicked.tryEmit(Unit)
     }
 
 }
